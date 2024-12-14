86ba8cfc7a382339963670112371243eae6f17f7
LTD <LTD@DESKTOP-PBCEB5J>
2024-12-14 18:45:47 +0700

    Add project files.

---
+++ LapTrinhWindows.Buoi6/Form1.cs
@@ -0,0 +1,42 @@
+﻿using LapTrinhWindows.Buoi6.Model;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LapTrinhWindows.Buoi6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Model1 model = new Model1();
+            var students = model.Students
+                  .Select(s => new
+                  {
+                      s.StudentID,
+                      s.FullName,
+                      s.AverageScore,
+                      FacultyName = s.Faculty.FacultyName
+                  })
+                  .ToList();
+
+            dataGridView1.DataSource = students;
+
+
+
+        }
+    }
+}
